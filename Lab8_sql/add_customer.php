1ac9363486e2ae2c6a1aec8092af109c75502f22
Poomrapee <poomrapee.8652@gmail.com>
2026-03-03 21:31:51 +0700

    .

--- Lab8_sql/add_customer.php
+++ Lab8_sql/add_customer.php
@@ -4,43 +4,43 @@
     <meta charset="UTF-8">
     <title>เพิ่มข้อมูลลูกค้า</title>
 </head>
-<boby>
+<body>
     <h2>เพิ่มข้อมูลลูกค้า</h2>
     <form action="insert_customer.php" method="POST">
     <label>ชื่อ :</label> <input type="text" name="Customer_Name" required><br>
     <label>นามสกุล :</label> <input type="text" name="Customer_Lastname" required><br>
 
-    <lable>เพศ :</lable>
+    <label>เพศ :</label>
     <input type="radio" name="Gender" value="ชาย">ชาย
     <input type="radio" name="Gender" value="หญิง">หญิง <br>
 
-    <lable>วัน-เดือน-ปี เกิด :</lable>
-    <input type="date" name="Birthdate"><br> <lable>ที่อยู่:</lable> <input type="text" name="Address" style="width: 300px;"><br>
+    <label>วัน-เดือน-ปี เกิด :</label>
+    <input type="date" name="Birthdate"><br> <label>ที่อยู่:</label> <input type="text" name="Address" style="width: 300px;"><br>
 
-    <lable>จังหวัด :</lable>
+    <label>จังหวัด :</label>
     <select name="Province">
-        <option valur="">- เลื่อกจังหวัด -</option>
-        <option valur="เชียงราย">เชียงราย</option>
-        <option valur="เชียงใหม่">เชียงใหม่</option>
-        <option valur="น่าน">น่าน</option>
-        <option valur="พะเยา">พะเยา</option>
-        <option valur="แพร่">แพร่</option>
-        <option valur="แม่ฮ่องสอน">แม่ฮ่องสอน</option>
-        <option valur="ลำปาง">ลำปาง</option>
-        <option valur="ลำพูน">ลำพูน</option>
-        <option valur="อุตรดิตถ์">อุตรดิตถ์</option>
+        <option value="">- เลื่อกจังหวัด -</option>
+        <option value="เชียงราย">เชียงราย</option>
+        <option value="เชียงใหม่">เชียงใหม่</option>
+        <option value="น่าน">น่าน</option>
+        <option value="พะเยา">พะเยา</option>
+        <option value="แพร่">แพร่</option>
+        <option value="แม่ฮ่องสอน">แม่ฮ่องสอน</option>
+        <option value="ลำปาง">ลำปาง</option>
+        <option value="ลำพูน">ลำพูน</option>
+        <option value="อุตรดิตถ์">อุตรดิตถ์</option>
     </select><br>
 
-    <lable>รหัสไปรษณีย์ :</lable> <input type="text" name="Zipcode" maxlength="5"><br>
-    <lable>โทรศัพท์ :</lable> <input type="text" name="Telephone"><br>
+    <label>รหัสไปรษณีย์ :</label> <input type="text" name="Zipcode" maxlength="5"><br>
+    <label>โทรศัพท์ :</label> <input type="text" name="Telephone"><br>
 
-    <lable>รายละเอียดอื่นๆ :</lable><br>
-    <textarea name="Customer_Description" row="4" cols="50"></textarea><br>
+    <label>รายละเอียดอื่นๆ :</label><br>
+    <textarea name="Customer_Description" rows="4" cols="50"></textarea><br>
 
     <fieldset>
         <legend>Account ของลูกค้า</legend>
-        <lable>Username :</lable> <input type="text" name="username" required><br>
-        <lable>Password :</lable> <input type="password" name="password" required><br>
+        <label>Username :</label> <input type="text" name="username" required><br>
+       <label>Password :</label> <input type="password" name="password" required><br>
         <label>Confirm Password :</label> <input type="password" name="confirm_password" required><br>
     </fieldset>
 
@@ -48,7 +48,7 @@
     <button type="submit">เพิ่มข้อมูลลูกค้า</button>
     <button type="reset">ยกเลิก</button>
 </form>
-</boby>
+</body>
 </html>
 
 
@@ -56,13 +56,28 @@
 include 'connect.php'; // เรียกใช้ไฟล์เชื่อมต่อที่เราทำไว้ก่อนหน้า
 
 // รับค่าจากฟอร์ม
+// รับค่าให้ครบทุกตัวแปรที่ส่งมาจาก form
 $name = $_POST['Customer_Name'];
 $lastname = $_POST['Customer_Lastname'];
-// รับค่าอื่นๆ ให้ครบตามฟอร์ม เช่น Gender, Age, Province, username, password...
+$gender = $_POST['Gender'];
+$birthdate = $_POST['Birthdate'];
+$address = $_POST['Address'];
+$province = $_POST['Province'];
+$zipcode = $_POST['Zipcode'];
+$telephone = $_POST['Telephone'];
+$description = $_POST['Customer_Description'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+// รับค่าอื่นๆ ให้ครบตามฟอร์ม เช่น Gender, Age, Province, username, password...
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 // คำสั่ง SQL สำหรับเพิ่มข้อมูล
-$sql = "INSERT INTO customer (Customer_Name, Customer_Lastname, Province, Telephone) 
-        VALUES ('$name', '$lastname', '$province', '$telephone')";
+$sql = "INSERT INTO customer (Customer_Name, Customer_Lastname, Gender, Birthdate, Address, Province, Zipcode, Telephone, Customer_Description, username, password) 
+        VALUES ('$name', '$lastname', '$gender', '$birthdate', '$address', '$province', '$zipcode', '$telephone', '$description', '$username', '$hashed_password')";
+
+if ($_POST['password'] !== $_POST['confirm_password']) {
+    die("รหัสผ่านไม่ตรงกัน! <a href='javascript:history.back()'>ย้อนกลับ</a>");
+}
 
 if (mysqli_query($conn, $sql)) {
     echo "บันทึกข้อมูลเรียบร้อยแล้ว!";
